21e39ace03c358abe1c29a96ca0a643985088178
zixt0 <zixt01337@gmail.com>
2020-03-03 22:30:55 +0100

    dbconnection Uden test

---
+++ WoddenLegs/PythonTest/DbConnectionTest.py
@@ -0,0 +1,18 @@
+import unittest
+
+from  DbLayer import DbConnection 
+class Test_connection(unittest.TestCase):
+    Dbconn = DbConnection
+
+
+    def test_Connection(self):
+        result =     
+        self.assert_
+
+
+        self.fail("Not implemented")
+    
+    if __name__ == '__main__':
+    unittest.main()
+
+    
