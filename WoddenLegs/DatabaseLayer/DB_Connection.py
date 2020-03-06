e747655a9848412184478443d7dcc1715f081981
zixt0 <zixt01337@gmail.com>
2020-03-06 11:33:11 +0100

    DB_Problemer

--- WoddenLegs/DatabaseLayer/DB_Connection.py
+++ WoddenLegs/DatabaseLayer/DB_Connection.py
@@ -1,55 +1,51 @@
 class DB_Connection(object):
-    """description of class"""
-
-
     import sqlite3
 
-conn = sqlite3.connect('dbwoddenlegs.db')
+    conn = sqlite3.connect('dbwoddenlegs.db')
 
-c = conn.cursor()
+    c = conn.cursor()
+    def db_check():
+        c.execute("""CREATE TABLE IF NOT EXISTS "RawData" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    "text"    TEXT NOT NULL,
+    "path"    TEXT NOT NULL,
+    "fileType"    TEXT NOT NULL
+);""" )
 
-c.execute("""CREATE TABLE IF NOT EXISTS "RawData" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"text"	TEXT NOT NULL,
-	"path"	TEXT NOT NULL,
-	"fileType"	TEXT NOT NULL
-);
-""" )
-
-c.execute("""CREATE TABLE IF NOT EXISTS "Number" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"path"	TEXT NOT NULL,
-	"identifier"	TEXT NOT NULL,
-	"id_raw"	INTEGER NOT NULL
+        c.execute("""CREATE TABLE IF NOT EXISTS "Number" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    "path"    TEXT NOT NULL,
+    "identifier"    TEXT NOT NULL,
+    "id_raw"    INTEGER NOT NULL
 );""")
 
-c.execute("""CREATE TABLE IF NOT EXISTS "Ip" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"path"	TEXT NOT NULL,
-	"identifier"	TEXT NOT NULL,
-	"id_rawData"	INTEGER
+        c.execute("""CREATE TABLE IF NOT EXISTS "Ip" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    "path"    TEXT NOT NULL,
+    "identifier"    TEXT NOT NULL,
+    "id_rawData"    INTEGER
 ); """)
 
-c.execute("""CREATE TABLE IF NOT EXISTS "Email" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"path"	TEXT NOT NULL,
-	"identifier"	TEXT NOT NULL,
-	"id_rawData"	INTEGER
+        c.execute("""CREATE TABLE IF NOT EXISTS "Email" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    "path"    TEXT NOT NULL,
+    "identifier"    TEXT NOT NULL,
+    "id_rawData"    INTEGER
 ); """)
 
-c.execute("""CREATE TABLE IF NOT EXISTS "BlacklistType" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"number"	NUMERIC CHECK(-1<number>=1),
-	"email"	NUMERIC CHECK(-1<email>=1),
-	"ip"	NUMERIC CHECK(-1<ip>=1)
+        c.execute("""CREATE TABLE IF NOT EXISTS "BlacklistType" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    "number"    NUMERIC CHECK(-1<number>=1),
+    "email"    NUMERIC CHECK(-1<email>=1),
+    "ip"    NUMERIC CHECK(-1<ip>=1)
 ); """)
 
-c.execute ("""CREATE TABLE IF NOT EXISTS "BlacklistKeyWord" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"keyWord"	TEXT NOT NULL,
-	"id_BlackListType"	INTEGER NOT NULL
+        c.execute ("""CREATE TABLE IF NOT EXISTS "BlacklistKeyWord" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    "keyWord"    TEXT NOT NULL,
+    "id_BlackListType"    INTEGER NOT NULL
 );""")
 
-conn.commit()
+        conn.commit()
 
-conn.close ()
+        conn.close ()
---
+++ WoddenLegs/DatabaseLayer/DB_PreparedStatements_RawData.py
@@ -0,0 +1,12 @@
+class DB_PreparedStatements_RawData(object):
+    """description of class"""
+    import sqlite3
+    import DB_Connection
+    DBconn = DB_Connection
+    DBconn.DB_Connection.db_check()
+    
+    def insert_querry():
+       
+
+    def insert_querry(s):
+        s.method_name().
