4596ba2b7b54ae497835142f44328fc9b71cdc96
Morten <mort381a@hotmail.com>
2020-03-09 23:52:21 +0100

    remove truee

--- WoddenLegs/PythonTest/DB_Test.py
+++ WoddenLegs/PythonTest/DB_Test.py
@@ -16,7 +16,6 @@ class IDB_Test(unittest.TestCase):
 class Test_DB_insert_Blacklist_type_test(unittest.TestCase):
     def test_querry_RawData(self):
         DB_BlacklistType.BlacklistType.update_ip_false()
-        print("true")
 
 class Test_test_1(unittest.TestCase):
     def test_A(self):
