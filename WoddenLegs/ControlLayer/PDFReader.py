d8993b430e0aefbb383cdd6029ef8732709749a2
denraskeko <psavag16@student.aau.dk>
2020-03-09 10:03:45 +0100

    Read image and read text methods combined and now output a dict of page number and text from the page

--- WoddenLegs/ControlLayer/PDFReader.py
+++ WoddenLegs/ControlLayer/PDFReader.py
@@ -5,57 +5,59 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tess
 import numpy as np
 from PIL import Image
 
-def readPDF():
-    file = open('ControlLayer\\TempPDFHolder\\report.pdf', 'rb') #Open PDF
-    fileReader = PyPDF4.PdfFileReader(file)
+class PdfReader:
+    def readImages(filepath):
+        file = open(filepath, 'rb') #Open PDF
+        fileReader = PyPDF4.PdfFileReader(file)
+        pageTextDict = {}
+        
 
-    page = fileReader.getPage(0) #Specify page to read
-
-    xObject = page['/Resources']['/XObject'].getObject() #Gets list of elements in page
-
-    for obj in xObject:
-        if xObject[obj]['/Subtype'] == '/Image': #If element is an image, resize and set color mode
-            size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-            data = xObject[obj].getData()
-            if(xObject[obj]['/ColorSpace'] == '/DeviceRGB'):
-                mode = "RGB"
-            else:
-                mode = "p"
-
+        for i in range(fileReader.getNumPages()): #Run this code once for each page in PDF file
+            page = fileReader.getPage(i) #Specify page to read
+            xObject = page['/Resources']['/XObject'].getObject() #Gets list of elements in page
+            text = ""
             
-            if xObject[obj]['/Filter'] == '/FlateDecode':
-                img = Image.frombytes(mode, size, data) #Decode bytes and create image
-                img.save(obj[1:] + ".png")
-                text = pytesseract.image_to_string(img) #Read any text in image
-                print(text)
+            for obj in xObject:
+                if xObject[obj]['/Subtype'] == '/Image': #If element is an image, resize and set color mode
+                    size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+                    data = xObject[obj].getData()
+                    if(xObject[obj]['/ColorSpace'] == '/DeviceRGB'):
+                        mode = "RGB"
+                    else:
+                        mode = "p"
+            
+                    if xObject[obj]['/Filter'] == '/FlateDecode':
+                        img = Image.frombytes(mode, size, data) #Decode bytes and create image
+                        img.save(obj[1:] + ".png")
+                        text += pytesseract.image_to_string(img) + " " #Read any text in image
                 
 
-            elif xObject[obj]['/Filter'] == '/DCTDecode':
-                img = open(obj[1:] + ".jpg", "wb")
-                img.write(data) #Save image in project directory
-                
-                #Change image type to opencv
-                img = Image.open('Im7.jpg').convert('RGB') #Open and convert image (this is hardcoded, needs to be fixed)
-                open_cv_image = np.array(img)
-                open_cv_image = open_cv_image[:, :, ::-1].copy()
+                    elif xObject[obj]['/Filter'] == '/DCTDecode':
+                        img = open(obj[1:] + ".jpg", "wb")
+                        img.write(data) #Save image in project directory
+                        img = Image.open(obj[1:] + ".jpg").convert('RGB') #Open and convert image (this is hardcoded, needs to be fixed)
+                        open_cv_image = np.array(img)
+                        open_cv_image = open_cv_image[:, :, ::-1].copy() #Change image type to opencv from PIL
+                        text += pytesseract.image_to_string(img) + " " #Read any text in image
+                        img.close()
 
-                text = pytesseract.image_to_string(img) #Read any text in image
-                print(text)
-                img.close()
+                    elif xObject[obj]['/Filter'] == '/JPXDecode':
+                        img = open(obj[1:] + ".jp2", "wb")
+                        img.write(data)
+                        #Change image type to opencv
+                        img = Image.open('Im7.jpg').convert('RGB')
+                        open_cv_image = np.array(img)
+                        open_cv_image = open_cv_image[:, :, ::-1].copy()
+                        text += pytesseract.image_to_string(img) + " " #Read any text in image
+                        img.close()
 
-            elif xObject[obj]['/Filter'] == '/JPXDecode':
-                img = open(obj[1:] + ".jp2", "wb")
-                img.write(data)
+            text += page.extractText()
+            pageTextDict[i] = text
 
-                #Change image type to opencv
-                img = Image.open('Im7.jpg').convert('RGB')
-                open_cv_image = np.array(img)
-                open_cv_image = open_cv_image[:, :, ::-1].copy() 
-
-                text = pytesseract.image_to_string(img) #Read any text in image
-                print(text)
-                img.close()
+        return pageTextDict
 
 
-
-readPDF()
+#Testing purposes only. Remove before release.
+pdfRead = PdfReader
+aDict = pdfRead.readImages('ControlLayer\\TempPDFHolder\\TestPDF2.pdf')
+print(aDict)
