fab1210c08fa9a81ecbf7820a8cada89d1fc564c
denraskeko <psavag16@student.aau.dk>
2020-03-17 12:20:32 +0100

    Unit tested regex classes

--- WoddenLegs/ControlLayer/IPChecker.py
+++ WoddenLegs/ControlLayer/IPChecker.py
@@ -2,7 +2,7 @@ import re
 
 class IPChecker:
 
-    def FindIP(text):
+    def findIP(text):
         IPV4SEG  = r'(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])'
         IPV4ADDR = r'(?:(?:' + IPV4SEG + r'\.){3,3}' + IPV4SEG + r')'
         IPV6SEG  = r'(?:(?:[0-9a-fA-F]){1,4})'
@@ -22,40 +22,5 @@ class IPChecker:
         )
         IPV6ADDR = '|'.join(['(?:{})'.format(g) for g in IPV6GROUPS[::-1]])  # Reverse rows for greedy match
         ips = re.findall(IPV6ADDR, text)
-        print(ips)
+        return ips
 
-tests = """1::
-1:2:3:4:5:6:7::
-1::8
-1:2:3:4:5:6::8
-1:2:3:4:5:6::8
-1::7:8
-1:2:3:4:5::7:8
-1:2:3:4:5::8
-1::6:7:8
-1:2:3:4::6:7:8
-1:2:3:4::8
-1::5:6:7:8
-1:2:3::5:6:7:8
-1:2:3::8
-1::4:5:6:7:8
-1:2::4:5:6:7:8
-1:2::8
-1::3:4:5:6:7:8
-1::3:4:5:6:7:8
-1::8
-::2:3:4:5:6:7:8
-::2:3:4:5:6:7:8
-::8
-::
-fe80::7:8%eth0
-fe80::7:8%1
-::255.255.255.255
-::ffff:255.255.255.255
-::ffff:0:255.255.255.255
-2001:db8:3:4::192.0.2.33
-64:ff9b::192.0.2.33
-hej"""
-
-ipCheck = IPChecker
-ipCheck.FindIP(tests)
--- WoddenLegs/ControlLayer/MailChecker.py
+++ WoddenLegs/ControlLayer/MailChecker.py
@@ -3,14 +3,10 @@ import re
 class MailChecker:
     
     def checkMail(text):
-        emails = re.findall('\S+@\S+\.[a-zA-Z]{2,4}\s', text)
+        emails = re.findall('\S+@\S+\.[a-zA-Z]{2,4}', text)
         print(emails)
         return emails
 
     def checkPhone(text):
         phones = re.search('(\+?(45){1})?([-.\s]?\d{1}){8}', text)
         print(phones)
-
-mailCheck = MailChecker
-mailCheck.checkPhone("4524233211")
-#mailCheck.checkMail("this is my email psavage1995@gmail.com and this is another one peop@babi.come s   hje@hotmail.cos.uk  abc@ads.co.ssdds")
