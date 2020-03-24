24b3528c0fc4d0800021befa7f45769b63d22d6c
denraskeko <psavag16@student.aau.dk>
2020-03-24 12:54:15 +0100

    Refactored main class and identifier so that identifiers are saved as a class with a few attributes. Added a new test pdf with a few identifiers in it

--- WoddenLegs/ControlLayer/Main2.py
+++ WoddenLegs/ControlLayer/Main2.py
@@ -1,15 +1,43 @@
 from ControlLayer.PDFReader import *
-class Main2(object):
-    """description of class"""
+from ControlLayer.RegexChecker import *
+from ModelLayer.Identifier import *
+import sys
+
+class Main2():
 
     def main():
         print("Running main method")
-        paths = ['ControlLayer\\TempPDFHolder\\OCRTestPDF.pdf', 'ControlLayer\\TempPDFHolder\\TestPDF2.pdf']
+        paths = ['ControlLayer\\TempPDFHolder\\TestPDF.pdf', 'ControlLayer\\TempPDFHolder\\TestPDF2.pdf']
 
+        for arg in sys.argv[1:]:
+            print(arg)
+
+        id = 0
+        identifiers = []
         for path in paths:
             textDict = PdfReader.readImages(path)
             for page in textDict:
-                print(textDict[page])
+                
+                emails = RegexChecker.checkMail(textDict[page])
+                for email in emails:
+                    ident = Identifier(email, "Email", path, page, id)
+                    identifiers.append(ident)
+                    id +=1
+                
+                phoneNumbers = RegexChecker.checkPhone(textDict[page])
+                for number in phoneNumbers:
+                    ident = Identifier(number, "PhoneNumber", path, page, id)
+                    identifiers.append(ident)
+                    id +=1
+
+                ips = RegexChecker.findIP(textDict[page])
+                for ip in ips:
+                    ident = Identifier(number, "IP", path, page, id)
+                    identifiers.append(ident)
+                    id +=1
+
+        for identifier in identifiers:
+            print("\nID: " + str(identifier.id) + "\nIdentifier: " + identifier.name + "\nPage:" + str(identifier.page) + "\nPath:" + identifier.path + "\nType:" + identifier.type)
 
     if __name__ == '__main__':
         main()
--- WoddenLegs/ModelLayer/Identifier.py
+++ WoddenLegs/ModelLayer/Identifier.py
@@ -1,7 +1,9 @@
 class Identifier():
 
-    def __init__(self, name, type):
+    def __init__(self, name, type, path, page, id):
         self.name = name
         self.type = type
-
+        self.path = path
+        self.page = page
+        self.id = id
 
