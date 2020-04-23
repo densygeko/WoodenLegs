d9d2bbd92b51a1651ea34c9e4d8cf779ad67a6ba
Sarah Wohlin <sarahwohlin@hotmail.com>
2020-04-23 12:01:11 +0200

    GUIinfolder

---
+++ GUI/InsertFiles.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for InsertFiles.xaml
+    /// </summary>
+    public partial class InsertFiles : Window
+    {
+        public InsertFiles()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (FolderBrowserDialog FBD = new FolderBrowserDialog())
+            {
+                string folderPath;
+                FBD.ShowDialog();
+                if (FBD.SelectedPath != null)
+                {
+                    folderPath = FBD.SelectedPath;
+                    Textbox.Text = folderPath;
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string path;
+            path = Textbox.Text;
+            FileController Fctr = new FileController();
+            DisplayIdentifier displayIdentifier = new DisplayIdentifier();
+            this.NavigationService.Navigate(displayIdentifier);
+
+        }
+        */
+
+    }
+}
---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Asks the user if they are sure that they want to close the application
+        private void ButtonCloseApplication_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Alt du har arbejdet på går tabt, er du sikker på at du vil lukke applikationen?", "", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    Application.Current.Shutdown();
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+        }
+
+
+        private void ButtonInsertDoc_Click(object sender, RoutedEventArgs e)
+        {
+            InsertFiles insertFilesWindow = new InsertFiles();
+            insertFilesWindow.Topmost = true;
+            insertFilesWindow.ShowDialog();
+            
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void blacklist_Click(object sender, RoutedEventArgs e)
+        {
+            if(BlackListData.Visibility == Visibility.Hidden)
+            {
+                BlackListData.Visibility = Visibility.Visible;
+                BlackListTextBox.Text = "Skjul blacklist";
+                BlackListIcon.Foreground = Brushes.Black;
+            }
+            else
+            {
+                BlackListData.Visibility = Visibility.Hidden;
+                BlackListTextBox.Text = "Vis blacklist";
+                BlackListIcon.Foreground = Brushes.White;
+                
+            }
+            
+        }
+    }
+}
