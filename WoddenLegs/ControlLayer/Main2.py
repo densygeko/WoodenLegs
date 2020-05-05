9e03cd07355f5eacaba0dac29b71f2fab1d81920
denraskeko <psavag16@student.aau.dk>
2020-05-05 11:39:22 +0200

    Each file type now parses on its own thread.

--- WoddenLegs/ControlLayer/Main2.py
+++ WoddenLegs/ControlLayer/Main2.py
@@ -6,17 +6,45 @@ from xml.dom import minidom
 import sys
 import os
 import ast
+from threading import Thread
 
 class Main2():
+
+    def __init__(self):
+        self.identifiers = []
 
     def main(self): #Needs to be updated to take a list of lists of paths
         if len(sys.argv) <= 1: #Must have at least one argument when executing this class in command line
             print("Please enter the path to the filepaths.xml document")
             return
         
+        XMLDoc = minidom.parse(str(sys.argv[1])) #Get XML doc from system argument provided from UI
+
+        #Create one thread for each file type in XML doc.
+        pdfThread = Thread(target=self.PdfThread, args=(XMLDoc.getElementsByTagName("pdfpath"),))     #The comma is added to the argument is seen as a tuple.
+        pcapThread = Thread(target=self.PcapThread, args=(XMLDoc.getElementsByTagName("pcappath"),))  #When a thread takes a list as an argument, it attempts
+        csvThread = Thread(target=self.CsvThread, args=(XMLDoc.getElementsByTagName("csvpath"),))     #to split the argument into multiple bits.
+        imgThread = Thread(target=self.ImgThread, args=(XMLDoc.getElementsByTagName("imgpath"),))
+        txtThread = Thread(target=self.TxtThread, args=(XMLDoc.getElementsByTagName("txtpath"),))
+
+        #Start Threads
+        pdfThread.start()
+        pcapThread.start()
+        csvThread.start()
+        imgThread.start()
+        txtThread.start()
+
+
+        #Wait for threads to finish
+        pdfThread.join()
+        pcapThread.join()
+        csvThread.join()
+        imgThread.join()
+        txtThread.join()
+
         xmlCreator = XMLCreator()
-        identifiers = self.SortFiles(sys.argv[1]) #Extract identifiers from text
-        matchedIdentifiers = self.MatchIdentifiers(identifiers) #Match duplicate identifiers
+        matchedIdentifiers = []
+        matchedIdentifiers.extend(self.MatchIdentifiers(self.identifiers)) #Match duplicate identifiers
         xmlCreator.CreateXMLDoc(matchedIdentifiers) #Create XML doc with matched identifiers data
 
     def MatchIdentifiers(self, identifiers):
@@ -41,71 +69,67 @@ class Main2():
                     matchedIdentifiers.append(match)
 
         return matchedIdentifiers
-
-    #Sort files into the right Parsers.
-    def SortFiles(self, xmlPath):
-        identifiers = []
-        print(str(xmlPath))
-        XMLDoc = minidom.parse(str(xmlPath)) #Get XML doc from system argument provided from UI
-        fileParser = FileParser()
-
+            
+    def PdfThread(self, pdfPaths):
         #.pdf files
+        fileParser = FileParser()
         try: 
-            pdfPaths = XMLDoc.getElementsByTagName("pdfpath") #Get pdfpath element from XML file
             pdfFiles = []
             for path in pdfPaths:
                 pdfFiles.append(path.firstChild.data) #Extract data from pdfpath element
         
-            identifiers.extend(fileParser.ParsePDF(pdfFiles))
+            self.identifiers.extend(fileParser.ParsePDF(pdfFiles))
         
         except:
             print("No .pdf files in directory or error related to .pdf")
 
+    def PcapThread(self, pcapPaths):
         #.pcap files
+        fileParser = FileParser()
         try:
-            pcapPaths = XMLDoc.getElementsByTagName("pcappath")
             pcapFiles = []
             for path in pcapPaths:
                 pcapFiles.append(path.firstChild.data)
 
-            identifiers.extend(fileParser.ParsePcap(pcapFiles))
+            self.identifiers.extend(fileParser.ParsePcap(pcapFiles))
         except:
             print("No .pcap files in directory or error related to .pcap")
 
+    def TxtThread(self, txtPaths):
         #.txt files
+        fileParser = FileParser()
         try:
-            txtPaths = XMLDoc.getElementsByTagName("txtpath")
             txtFiles = []
             for path in txtPaths:
                 txtFiles.append(path.firstChild.data)
 
-            identifiers.extend(fileParser.ParseTxt(txtFiles))
+            self.identifiers.extend(fileParser.ParseTxt(txtFiles))
         except:
             print("No .txt files in directory or error related to .txt")
 
+    def CsvThread(self, csvPaths):
         #.csv files
+        fileParser = FileParser()
         try:
-            csvPaths = XMLDoc.getElementsByTagName("csvpath")
             csvFiles = []
             for path in csvPaths:
                 csvFiles.append(path.firstChild.data)
 
-            identifiers.extend(fileParser.ParseCsv(csvFiles))
+            self.identifiers.extend(fileParser.ParseCsv(csvFiles))
         except:
             print("No .csv files in directory or error related to .csv")
 
+    def ImgThread(self, imgPaths):
         #Images
+        fileParser = FileParser()
         try:
-            imgPaths = XMLDoc.getElementsByTagName("imgpath")
             imgFiles = []
             for path in imgPaths:
                 imgFiles.append(path.firstChild.data)
 
-            identifiers.extend(fileParser.ParseImg(imgFiles))
+            self.identifiers.extend(fileParser.ParseImg(imgFiles))
         except:
             print("No images found in directory or error related to images")
-
-        return identifiers
 
 if __name__ == '__main__':
     mainClass = Main2()
